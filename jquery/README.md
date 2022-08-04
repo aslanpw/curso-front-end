e0e160edb7a8332fb3e0dd051d8112a882210324
Breno Valentim <contato.brvalentim@outlook.com>
2022-08-04 09:45:44 -0300

    Iniciando -> HTML -> CSS -> Bootstrap -> Jquery

---
+++ jquery/README.md
@@ -0,0 +1,27 @@
+# JQuery
+"write less, do more"
+- Biblioteca de JavaScript
+- Carregamento Rapido
+- Converte para JS (EcmaScript) padrão
+- Crossbrowser: funciona em diversos navegadores
+- Escrever um único código para todos os navegadores
+- Rápida captura e transmissão de dados
+- Manipula o DOM
+- Facilita a consulta (query) a elementos
+- Extensível com plugins
+- Instalação: htttps://jquery.com/download/
+
+## Seletores
+### Simples
+'''
+    $('h4') // tag
+    $('.featured-item') // class
+    $('#featured') // id
+'''
+
+### Compostos
+'''
+    $('h4, h6')
+
+    $('div h4')
+'''
---
+++ jquery/js/custom.js
@@ -0,0 +1,39 @@
+// instancia jquery e evita conflitos
+//jQuery (function($) {
+
+jQuery(function($){
+
+    $('.owl-carousel').owlCarousel();
+
+    let titulos  = $('h4') // tag
+
+    let itens = $('.featured-item') // class
+
+    let destaques = $('#featured') // id
+
+    console.log(titulos.first());
+
+    // Configuração de produtos
+
+    $('.featured-item a').addClass('btn btn-secondary stretch-link');
+
+    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>');
+
+    $('.featured-item h4').dblclick( function(){
+
+        $(this).css({
+            'color': '#f00',
+            'background' : '#ff0',
+            'font-weight' : '100',
+        });
+
+    });
+
+    $('.featured-item .comprar').on('click', function(event){
+        event.preventDefault();
+
+        alert('Produto esgotado');
+
+    });
+
+})
